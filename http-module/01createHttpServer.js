0f89743a944d111c600fe4cfe827ed739cbe8eb3
Tay Kong Yu <kongyutay@gmail.com>
2024-06-21 17:34:29 +0800

    set header to decode chinese

--- http-module/01createHttpServer.js
+++ http-module/01createHttpServer.js
@@ -8,7 +8,9 @@ const server = http.createServer((request, response) => {
 
     //response也是一个对象，对响应报文的封装对象
     //可以为浏览器设置响应结果
-    response.end('Hello HTTP Server');
+    //如果返回中文，要设置响应报头，否则会出现乱码，让它用utf-8解析中文内容
+    response.setHeader("content-type", "text/html;charset=utf-8");
+    response.end('你好');
     //这是为了设置响应体，并结束这个响应
 });
 
